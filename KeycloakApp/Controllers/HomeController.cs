63247831e5e1743f43f464afd28de41718d0c5d4
Behzad Parsa <behzad.parsa1996@yahoo.com>
2023-09-21 23:18:16 +0330

    Implementation of callback method

--- KeycloakApp/Controllers/HomeController.cs
+++ KeycloakApp/Controllers/HomeController.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.Mvc;
 
 namespace KeycloakApp.Controllers
@@ -13,6 +17,7 @@ namespace KeycloakApp.Controllers
             return View();
         }
 
+        [Authorize]
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -28,7 +33,26 @@ namespace KeycloakApp.Controllers
         }
         public ActionResult Callback()
         {
-            return Content("");
+            //in web api: HttpContext.Current.Request.Headers
+            var accessToken = HttpContext.Request.Headers["Authorization"];
+            var refreshToken = HttpContext.Request.Headers["RefreshToken"];
+            var decodedToken = Helper.DecodeToken(accessToken);
+            var username = decodedToken.Claims.FirstOrDefault(x => x.Type == "preferred_username").Value;
+            var fullname = decodedToken.Claims.FirstOrDefault(x => x.Type == "name").Value;
+            var claims = new[]
+            {
+                new Claim("UserName",username),
+                new Claim("FullName",fullname),
+                new Claim("AccessToken",accessToken.ToString()),
+                new Claim("RefreshToken",refreshToken.ToString()),
+            };
+
+            var identity = new ClaimsIdentity(claims, "keycloak_sso_auth");
+
+            Request.GetOwinContext().Authentication.SignIn(identity);
+            return RedirectToAction("About");
         }
+
+
     }
 }
---
+++ KeycloakApp/Helper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Web;
+
+namespace KeycloakApp
+{
+    public class Helper
+    {
+        public static JwtSecurityToken DecodeToken(string token)
+        {
+            var handler = new JwtSecurityTokenHandler();
+            var jsonToken = handler.ReadToken(token);
+            return jsonToken as JwtSecurityToken;
+        }
+
+    }
+}
